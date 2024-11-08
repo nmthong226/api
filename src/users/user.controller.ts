3aab6286081ab7148c521468a2000fe893661c9d
nmthong226 <nmthong226@gmail.com>
2024-11-08 23:55:11 +0700

    feat: get user profile

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,16 +1,20 @@
 // src/users/users.controller.ts
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Req, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) { }
-
     @UseGuards(JwtAuthGuard) // Protect the route with JWT
     @Get('profile')
     async getProfile(@Req() req) {
-        // `req.user` contains the user ID and username from the decoded token
-        return this.usersService.getUserProfile(req.user.userId);
+      // `req.user` contains the decoded payload from the token (e.g., userId and email)
+      const user = await this.usersService.getUserProfile(req.user.sub);
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
+      return user;
     }
+    
 }
