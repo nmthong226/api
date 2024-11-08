eb716417d994db49de726b69bc2e9bc49a64b744
nmthong226 <nmthong226@gmail.com>
2024-11-08 23:54:29 +0700

    fix: module config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -19,6 +19,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         secret: configService.get<string>('JWT_SECRET'),
         signOptions: { expiresIn: '1h' },
       }),
+      global: true,
       inject: [ConfigService],
     }),
   ],
